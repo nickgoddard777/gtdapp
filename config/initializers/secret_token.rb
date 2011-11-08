9cedb002849f075c951d9dd4ad4003b3373826e7
Nick Goddard <nicko777@gmail.com>
2011-11-08 20:22:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtdapp::Application.config.secret_token = '0e5465233652a6ad6cab22ed64f099438c0bb342887efe3f49dab72a9903c990aa8df298bacb2dd8d410d6aace44dc0e54b001386432ece38be5031859fe9187'
